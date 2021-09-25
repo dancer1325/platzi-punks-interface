1214141ea8288d79ad39c01e69ab5633c1f5d0bc
ernestognw <ernestognw@gmail.com>
2021-09-25 17:52:44 -0500

    class(2): Connect injected provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import Web3 from "web3";
 import { Route } from "react-router-dom";
 import Home from "./views/home";
 
 const App = () => {
+  useEffect(() => {
+    if (window.ethereum) {
+      // window.ethereum
+      //   .request({ method: "eth_requestAccounts" })
+      //   .then(console.log);
+
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  }, []);
+
   return (
     <>
       <Route path="/" exact component={Home} />
