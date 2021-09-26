f376ee85f01007d8a4070f90bdb3ab5f06d11d3c
ernestognw <ernestognw@gmail.com>
2021-09-25 20:08:31 -0500

    class(10): Filter PlatziPunks by account

--- src/hooks/usePunksData/index.js
+++ src/hooks/usePunksData/index.js
@@ -1,3 +1,4 @@
+import { useWeb3React } from "@web3-react/core";
 import { useCallback, useEffect, useState } from "react";
 import usePlatziPunks from "../usePlatziPunks";
 
@@ -63,8 +64,9 @@ const getPunkData = async ({ platziPunks, tokenId }) => {
   };
 };
 
-const usePunksData = () => {
+const usePunksData = ({ owner = null } = {}) => {
   const [punks, setPunks] = useState([]);
+  const { library } = useWeb3React();
   const [loading, setLoading] = useState(true);
   const platziPunks = usePlatziPunks();
 
@@ -74,8 +76,20 @@ const usePunksData = () => {
 
       let tokenIds;
 
-      const totalSupply = await platziPunks.methods.totalSupply().call();
-      tokenIds = new Array(Number(totalSupply)).fill().map((_, index) => index);
+      if (!library.utils.isAddress(owner)) {
+        const totalSupply = await platziPunks.methods.totalSupply().call();
+        tokenIds = new Array(Number(totalSupply))
+          .fill()
+          .map((_, index) => index);
+      } else {
+        const balanceOf = await platziPunks.methods.balanceOf(owner).call();
+        const tokenIdsOfOwner = new Array(Number(balanceOf))
+          .fill()
+          .map((_, index) =>
+            platziPunks.methods.tokenOfOwnerByIndex(owner, index).call()
+          );
+        tokenIds = await Promise.all(tokenIdsOfOwner);
+      }
 
       const punksPromise = tokenIds.map((tokenId) =>
         getPunkData({ tokenId, platziPunks })
@@ -86,7 +100,7 @@ const usePunksData = () => {
       setPunks(punks);
       setLoading(false);
     }
-  }, [platziPunks]);
+  }, [platziPunks, owner, library?.utils]);
 
   useEffect(() => {
     update();
--- src/views/punks/index.js
+++ src/views/punks/index.js
@@ -1,19 +1,81 @@
 import { useWeb3React } from "@web3-react/core";
-import { Grid } from "@chakra-ui/react";
+import {
+  Grid,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  InputRightElement,
+  Button,
+  FormHelperText,
+  FormControl,
+} from "@chakra-ui/react";
 import { usePunksData } from "../../hooks/usePunksData";
 import RequestAccess from "../../components/request-access";
 import Loading from "../../components/loading";
 import PunkCard from "../../components/punk-card";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
+import { useState } from "react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 const Punks = () => {
-  const { active } = useWeb3React();
-  const { punks, loading } = usePunksData();
+  const { search } = useLocation();
+  const { push } = useHistory();
+  const [validAddress, setValidAddress] = useState(true);
+  const [submitted, setSubmitted] = useState(true);
+  const [address, setAddress] = useState(
+    new URLSearchParams(search).get("address")
+  );
+  const { active, library } = useWeb3React();
+  const { punks, loading } = usePunksData({
+    owner: submitted && validAddress ? address : null,
+  });
+
+  const handleAddressChange = ({ target: { value } }) => {
+    setAddress(value);
+    setValidAddress(false);
+    setSubmitted(false);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    if (address) {
+      const isValid = library.utils.isAddress(address);
+      setValidAddress(isValid);
+      setSubmitted(true);
+      if (isValid) push(`/punks?address=${address}`);
+    } else {
+      push("/punks");
+    }
+  };
 
   if (!active) return <RequestAccess />;
 
   return (
     <>
+      <form onSubmit={submit}>
+        <FormControl>
+          <InputGroup mb={3}>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<SearchIcon color="gray.300" />}
+            />
+            <Input
+              isInvalid={!validAddress}
+              value={address ?? ""}
+              onChange={handleAddressChange}
+              placeholder="Buscar por dirección"
+            />
+            <InputRightElement width="5.5rem">
+              <Button type="submit" h="1.75rem" size="sm">
+                Buscar
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          {submitted && !validAddress && (
+            <FormHelperText>Dirección inválida</FormHelperText>
+          )}
+        </FormControl>
+      </form>
       {loading ? (
         <Loading />
       ) : (
