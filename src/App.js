8cc17470b5ace374fd52912c3004c8b9b5732d63
ernestognw <ernestognw@gmail.com>
2021-09-25 19:48:48 -0500

    class(7): Adding PlatziPunks gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route } from "react-router-dom";
 import MainLayout from "./layouts/main";
 import Home from "./views/home";
+import Punks from "./views/punks";
 
 const App = () => {
   return (
     <MainLayout>
       <Route path="/" exact component={Home} />
+      <Route path="/punks" exact component={Punks} />
     </MainLayout>
   );
 };
---
+++ src/hooks/usePunksData/index.js
@@ -0,0 +1,102 @@
+import { useCallback, useEffect, useState } from "react";
+import usePlatziPunks from "../usePlatziPunks";
+
+const getPunkData = async ({ platziPunks, tokenId }) => {
+  const [
+    accessoriesType,
+    clotheColor,
+    clotheType,
+    eyeType,
+    eyeBrowType,
+    facialHairColor,
+    facialHairType,
+    hairColor,
+    hatColor,
+    graphicType,
+    mouthType,
+    skinColor,
+    topType,
+    tokenURI,
+    dna,
+    owner,
+  ] = await Promise.all([
+    platziPunks.methods.getAccessoriesType(tokenId).call(),
+    platziPunks.methods.getClotheColor(tokenId).call(),
+    platziPunks.methods.getClotheType(tokenId).call(),
+    platziPunks.methods.getEyeType(tokenId).call(),
+    platziPunks.methods.getEyeBrowType(tokenId).call(),
+    platziPunks.methods.getFacialHairColor(tokenId).call(),
+    platziPunks.methods.getFacialHairType(tokenId).call(),
+    platziPunks.methods.getHairColor(tokenId).call(),
+    platziPunks.methods.getHatColor(tokenId).call(),
+    platziPunks.methods.getGraphicType(tokenId).call(),
+    platziPunks.methods.getMouthType(tokenId).call(),
+    platziPunks.methods.getSkinColor(tokenId).call(),
+    platziPunks.methods.getTopType(tokenId).call(),
+    platziPunks.methods.tokenURI(tokenId).call(),
+    platziPunks.methods.tokenDNA(tokenId).call(),
+    platziPunks.methods.ownerOf(tokenId).call(),
+  ]);
+  const response = await fetch(tokenURI);
+  const metadata = await response.json();
+
+  return {
+    tokenId,
+    attributes: {
+      accessoriesType,
+      clotheColor,
+      clotheType,
+      eyeType,
+      eyeBrowType,
+      facialHairColor,
+      facialHairType,
+      hairColor,
+      hatColor,
+      graphicType,
+      mouthType,
+      skinColor,
+      topType,
+    },
+    owner,
+    dna,
+    ...metadata,
+  };
+};
+
+const usePunksData = () => {
+  const [punks, setPunks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const platziPunks = usePlatziPunks();
+
+  const update = useCallback(async () => {
+    if (platziPunks) {
+      setLoading(true);
+
+      let tokenIds;
+
+      const totalSupply = await platziPunks.methods.totalSupply().call();
+      tokenIds = new Array(Number(totalSupply)).fill().map((_, index) => index);
+
+      const punksPromise = tokenIds.map((tokenId) =>
+        getPunkData({ tokenId, platziPunks })
+      );
+
+      const punks = await Promise.all(punksPromise);
+
+      setPunks(punks);
+      setLoading(false);
+    }
+  }, [platziPunks]);
+
+  useEffect(() => {
+    update();
+  }, [update]);
+
+  return {
+    loading,
+    punks,
+    update,
+  };
+};
+
+export { usePunksData };
